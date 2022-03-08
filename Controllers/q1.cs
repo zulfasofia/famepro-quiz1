a7d1eb873bcd5af6d17f815b97c50b1809d84721
Zulfa Solichin <zulfasofia@gmail.com>
2022-03-08 10:16:01 +0700

    Add project files.

---
+++ Controllers/q1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace startup1.Controllers
+{
+    public class q1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult profile()
+        {
+            return View();
+        }
+
+        public IActionResult company()
+        {
+            return View();
+        }
+
+        public IActionResult product()
+        {
+            return View();
+        }
+
+        public IActionResult collab()
+        {
+            return View();
+        }
+    }
+}
